81c0528b7d1516ff0e68cca48b3b553a79ac2ecb
ethanliwei <43132203+ethanliwei@users.noreply.github.com>
2018-09-13 12:38:12 +0800

    Update top_10.py

--- top_10.py
+++ top_10.py
@@ -1,1 +1,21 @@
 # This scrip is use to list top 10 frequency words in a txt file 
+from sys import argv
+from os.path import exists
+
+scrip, input_file = argv
+
+# First, you should copy all words of a file to input.txt
+print "Dose the input.txt exist? %r" % exists(input_file)
+print "if false then you should creat a input.txt"
+
+in_file = open(input_file)
+all_words = in_file.read()
+
+words_list = all_words.split()
+
+word_freq = [words_list.count(w) for w in words_list]
+
+print("Words:\n {} \n".format(all_words))
+print("List:\n {} \n".format(str(words_list)))
+print("Frequencies:\n {} \n".format(str(word_freq)))
+print("Pairs:\n {}".format(str(dict(zip(words_list, word_freq)))))
